05d15f8863b85d291240aabb9bba5325c159bfce
Finn <185564612+finn768@users.noreply.github.com>
2026-04-29 14:55:17 +0100

    update

--- src/app/whats-on/productions.tsx
+++ src/app/whats-on/productions.tsx
@@ -111,7 +111,7 @@ export default function UOBTheatreProductions(): JSX.Element {
 
   return (
     <StaTextContainer
-      className={"grid grid-cols-1 sm:grid-cols-2 grid-flow-row border border-white"}
+      className={"grid grid-cols-1 sm:grid-cols-2 grid-flow-row"}
     >
       {edges.map((edge) => (
         <ProductionTile key={edge.node.slug} production={edge.node} />
